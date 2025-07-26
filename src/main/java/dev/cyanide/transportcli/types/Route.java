e5b33114a0eafa48340a493ca790af9638dd2f92
anisul islam <anis.2803@gmail.com>
2025-07-26 23:10:01 +0200

    add route pojo

--- src/main/java/dev/cyanide/transportcli/types/Route.java
+++ src/main/java/dev/cyanide/transportcli/types/Route.java
@@ -2,10 +2,36 @@ package dev.cyanide.transportcli.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Route {
 
     long uniqueId;
     double distance;
+    List<RoutePart> parts;
 
+    public long getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(long uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public List<RoutePart> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<RoutePart> parts) {
+        this.parts = parts;
+    }
 }
