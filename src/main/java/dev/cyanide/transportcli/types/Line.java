99698f8c99afe86065d9fae5f1aa69fafe91985a
anisul islam <anis.2803@gmail.com>
2025-07-27 23:29:05 +0200

    add getter and setter for Line

--- src/main/java/dev/cyanide/transportcli/types/Line.java
+++ src/main/java/dev/cyanide/transportcli/types/Line.java
@@ -12,4 +12,52 @@ public class Line {
     String network;
     boolean sev;
     TransportType transportType;
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDivaId() {
+        return divaId;
+    }
+
+    public void setDivaId(String divaId) {
+        this.divaId = divaId;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNetwork() {
+        return network;
+    }
+
+    public void setNetwork(String network) {
+        this.network = network;
+    }
+
+    public boolean isSev() {
+        return sev;
+    }
+
+    public void setSev(boolean sev) {
+        this.sev = sev;
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
 }
