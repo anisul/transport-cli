84d38a3aab59c8f5b26c8e44d418278831307240
anisul islam <anis.2803@gmail.com>
2025-07-28 23:35:40 +0200

    optimize import

--- src/main/java/dev/cyanide/transportcli/types/Line.java
+++ src/main/java/dev/cyanide/transportcli/types/Line.java
@@ -1,7 +1,6 @@
 package dev.cyanide.transportcli.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Line {
